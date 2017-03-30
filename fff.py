35cb1fddaebacbcff939ac0716caf72ed4350f95
Dominic Fung <domokun997@gmail.com>
2017-03-31 09:52:34 +1100

    Change default route for / to home page
    Also update shebang for portability

--- fff.py
+++ fff.py
@@ -1,13 +1,14 @@
-#!/usr/bin/python
-#Written by Costa Paraskevopoulos in March 2017
-#Implements a prototype for Fine Food Finder
+#!/usr/bin/env python3
+# SENG2011 17s1 Project
+# By: team centipede
+# Implements a prototype for Fine Food Finder
 
 import os, re
-from flask import *
+from flask import Flask, request, render_template, send_from_directory, redirect, url_for
 
 #create new flask app
 app = Flask(__name__)
-app.secret_key = os.urandom(1)
+app.secret_key = os.urandom(1) # TODO replace with a single secret_key
 
 #register page
 @app.route('/register', methods=['GET', 'POST'])
@@ -21,39 +22,41 @@ def register():
 		pass2 = request.form.get('pass2')
 		email = request.form.get('email')
 
-		if 'cancel' in request.form:
-			#cancel registration
+		if 'cancel' in request.form: # Cancel registration, return to login page
 			return redirect(url_for('login'))
 		elif user == '' or pass1 == '' or pass2 == '' or email == '':
-			#no blank fields allowed
+			# no blank fields allowed
 			return render_template('register.html', status='Fields marked with (*) are required.')
 		elif not re.match(r'.+@.+', email):
 			return render_template('register.html', status='Invalid email.')
 		elif pass1 != pass2:
 			return render_template('register.html', status='Passwords do not match.')
 		else:
-			#correct registration details
-			flash('Confirmation email send to ' + email + '.')
+			# correct registration details
+			flash('Confirmation email sent to ' + email + '.')
 			return redirect(url_for('login'))
 
 #landing page
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'GET':
-		#first page load
+	if request.method == 'GET': # first page load
 		return render_template('login.html', status='')
 	else:
-		#user has pressed login button
+		# user has pressed login button
 		username = request.form.get('user')
 		password = request.form.get('pass')
 
-		#check username & password
+		# check username & password
 		if username != 'costa' or password != 'qwerty':
 			return render_template('login.html', status='Invalid username or password.')
 		else:
 			return render_template('home.html')
 
-#used to serve files in static dir
+# Serve static files from static/
 @app.route('/static/<path:path>')
 def send_static_file(path):
 	return send_from_directory('static', path)
