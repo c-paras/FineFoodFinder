6691fa7c63b079b50c7ac7b7e527c83eabf7b2cb
Dominic Fung <domokun997@gmail.com>
2017-03-31 10:06:04 +1100

    Reorder file and update comments

--- fff.py
+++ fff.py
@@ -10,11 +10,31 @@ from flask import Flask, request, render_template, send_from_directory, redirect
 app = Flask(__name__)
 app.secret_key = os.urandom(1) # TODO replace with a single secret_key
 
-#register page
+# Landing page
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+# Login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'GET': # First page load
+		return render_template('login.html', status='')
+	else:
+		# User has pressed login button
+		username = request.form.get('user')
+		password = request.form.get('pass')
+
+		# Check username & password
+		if username != 'costa' or password != 'qwerty':
+			return render_template('login.html', status='Invalid username or password.')
+		else:
+			return render_template('home.html')
+
+# Registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-	if request.method == 'GET':
-		#first page load
+	if request.method == 'GET': # Load registration form
 		return render_template('register.html', status='')
 	else:
 		user  = request.form.get('user')
@@ -25,36 +45,16 @@ def register():
 		if 'cancel' in request.form: # Cancel registration, return to login page
 			return redirect(url_for('login'))
 		elif user == '' or pass1 == '' or pass2 == '' or email == '':
-			# no blank fields allowed
+			# No blank fields allowed
 			return render_template('register.html', status='Fields marked with (*) are required.')
 		elif not re.match(r'.+@.+', email):
 			return render_template('register.html', status='Invalid email.')
 		elif pass1 != pass2:
 			return render_template('register.html', status='Passwords do not match.')
 		else:
-			# correct registration details
+			# Correct registration details
 			flash('Confirmation email sent to ' + email + '.')
 			return redirect(url_for('login'))
-
-#landing page
-@app.route('/')
-def home_page():
-    return render_template('home.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	if request.method == 'GET': # first page load
-		return render_template('login.html', status='')
-	else:
-		# user has pressed login button
-		username = request.form.get('user')
-		password = request.form.get('pass')
-
-		# check username & password
-		if username != 'costa' or password != 'qwerty':
-			return render_template('login.html', status='Invalid username or password.')
-		else:
-			return render_template('home.html')
 
 # Serve static files from static/
 @app.route('/static/<path:path>')
@@ -63,3 +63,4 @@ def send_static_file(path):
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
+
