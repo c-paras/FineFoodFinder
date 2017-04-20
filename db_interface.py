06e296d0d670799fe9bf8e3c87a7cbf71c4d6f3d
leagueofcake <domokun997@gmail.com>
2017-04-20 16:56:00 +1000

    Implement search_restaurants (name, cuisine, cost, suburb, rating) and tests, add __eq__ method for Restaurant ans User

--- db_interface.py
+++ db_interface.py
@@ -20,3 +20,26 @@ def get_restaurants(c):
         )
         results.append(r)
     return results
+
+
+def search_restaurants(c, criteria="", search_term="", search_term2=""):
+    """
+    Search for restaurants by specified criteria.
+
+    Text searches: returns restaurants where search_term in lowercase characters is found in the required field
+    Number searches (rating, cost etc.): returns restaurants where (search_term <= field <= search_term2)
+
+    search_term2 is only used for number searches.
+    """
+    results = []
+    restaurants = get_restaurants(c)
+    for r in restaurants:
+        name_search     = criteria == "name"    and search_term.lower() in r.get_name().lower()
+        cuisine_search  = criteria == "cuisine" and search_term.lower() in r.get_cuisine().lower()
+        cost_search     = criteria == "cost"    and search_term <= r.get_cost() <= search_term2
+        suburb_search   = criteria == "suburb"  and search_term.lower() in r.get_suburb().lower()
+        rating_search   = criteria == "rating"  and search_term <= r.get_rating() <= search_term2
+
+        if name_search or cuisine_search or cost_search or suburb_search or rating_search:
+            results.append(r)
+    return results
